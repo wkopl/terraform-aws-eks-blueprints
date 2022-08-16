ef03af40bcb82793e74c194d87f63f7942292698
Bryant Biggs <bryantbiggs@gmail.com>
2022-08-16 19:24:58 -0400

    chore: stash work in progress (TBD)

--- modules/kubernetes-addons/cluster-autoscaler/main.tf
+++ modules/kubernetes-addons/cluster-autoscaler/main.tf
@@ -12,7 +12,7 @@ module "helm_addon" {
   helm_config = merge({
     name        = local.name
     chart       = local.name
-    version     = "9.19.1"
+    version     = "9.19.3"
     repository  = "https://kubernetes.github.io/autoscaler"
     namespace   = local.namespace
     description = "Cluster AutoScaler helm Chart deployment configuration."
